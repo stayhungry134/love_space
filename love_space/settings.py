5f12372e4b6d6c0a8dc2035f94e2c24d622b2ce0
Ethan <stayhungry134@gmail.com>
2023-03-09 00:07:52 +0800

    修改了静态文件根路径

--- love_space/settings.py
+++ love_space/settings.py
@@ -122,8 +122,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = ['static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
