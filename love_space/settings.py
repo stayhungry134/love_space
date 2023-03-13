7ab617c5f807f37816ed97b87732ad396b1122f4
Ethan <stayhungry134@gmail.com>
2023-03-13 20:28:26 +0800

    修改了静态文件配置

--- love_space/settings.py
+++ love_space/settings.py
@@ -123,7 +123,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ['static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
